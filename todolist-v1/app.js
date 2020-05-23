ffab11c0000b21a0cd335b2c8f5162dfcc68d1de
Taewook Kang <dongnae.kang@gmail.com>
2020-05-23 14:20:54 +0900

    Todo Lists done with EJS and templates

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 const app = express();
 
-let items = [];
+const items = [];
+const workItems = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -10,25 +12,38 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res) {
 
-    let today = new Date();
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
+    let day = date.getDate();
 
-    let day = today.toLocaleDateString("en-US", options);
+    res.render("list", {listTitle: day, newListItems: items});
+});
 
-    res.render("list", {kindOfDay: day, newListItems: items});
+app.get("/work", function(req, res) {
+    res.render("list", {listTitle: "Work List", newListItems: workItems})
+});
+
+app.get("/about", function(req, res) {
+    res.render("about");
 });
 
 app.post("/", function(req, res) {
-    let item = req.body.newItem;
 
-    items.push(item);
+    const item = req.body.newItem;
 
-    res.redirect("/");
+    if(req.body.list == "Work List") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
 });
+
+app.post("/work", function(req, res) {
+    const item = req.body.newItem;
+    workItems.push(newItem);
+
+    res.redirect("/work");
+})
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
